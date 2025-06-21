76b0c1b41036d9c1086feca5441128511dda80d4
banksean <banksean@gmail.com>
2025-06-21 01:17:10 +0000

    add comprehensive unit tests
    Co-Authored-By: sketch <hello@sketch.dev>
    Change-ID: s2ef118ce8f05ae43k

--- main.go
+++ main.go
@@ -3,26 +3,55 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
-func main() {
-	var name string
-	var verbose bool
+// Config holds the application configuration
+type Config struct {
+	Name    string
+	Verbose bool
+}
 
-	flag.StringVar(&name, "name", "World", "Name to greet")
-	flag.BoolVar(&verbose, "verbose", false, "Enable verbose output")
+// parseFlags parses command line flags and returns a Config
+func parseFlags() *Config {
+	var config Config
+	flag.StringVar(&config.Name, "name", "World", "Name to greet")
+	flag.BoolVar(&config.Verbose, "verbose", false, "Enable verbose output")
 	flag.Parse()
+	return &config
+}
 
-	if verbose {
-		fmt.Printf("Greeting %s...\n", name)
+// formatGreeting generates the greeting message
+func formatGreeting(name string) string {
+	return fmt.Sprintf("Hello, %s!", name)
+}
+
+// formatVerbosePrefix generates the verbose prefix message
+func formatVerbosePrefix(name string) string {
+	return fmt.Sprintf("Greeting %s...", name)
+}
+
+// formatVerboseSuffix generates the verbose suffix message
+func formatVerboseSuffix() string {
+	return "Done!"
+}
+
+// greet outputs the greeting messages to the provided writer
+func greet(w io.Writer, config *Config) {
+	if config.Verbose {
+		fmt.Fprintln(w, formatVerbosePrefix(config.Name))
 	}
 
-	fmt.Printf("Hello, %s!\n", name)
+	fmt.Fprintln(w, formatGreeting(config.Name))
 
-	if verbose {
-		fmt.Println("Done!")
+	if config.Verbose {
+		fmt.Fprintln(w, formatVerboseSuffix())
 	}
+}
 
+func main() {
+	config := parseFlags()
+	greet(os.Stdout, config)
 	os.Exit(0)
 }
