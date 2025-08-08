1f17019b8d89569d4e1d511b09f418b05173cff3
banksean <banksean@gmail.com>
2025-08-08 23:01:51 +0000

    add hello world web server in go
    Co-Authored-By: sketch <hello@sketch.dev>
    Change-ID: s8c67df0bd3ddbaeek

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	port := ":8080"
+	log.Printf("Starting server on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
